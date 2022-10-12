b47eb15c4c4d7517ce1eadc03940ff0407f78813
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-10-12 22:47:02 +0530

    [Added: Showing board]

--- src/com/tictactoa/TicTacToe.java
+++ src/com/tictactoa/TicTacToe.java
@@ -1,19 +1,21 @@
 package com.tictactoa;
+
 import java.util.*;
+
 public class TicTacToe {
 
 	int i;
-	static char[] board = new char[10];//taking array
+	static char[] board = new char[10];// taking array
 	static char player, computer;
-	Scanner scan = new Scanner(System.in);//creating object
+	Scanner scan = new Scanner(System.in);// creating object
 
 	public void initialize() {
-		for (i = 1; i < 10; i++) {   //initialize
+		for (i = 1; i < 10; i++) { // initialize
 			board[i] = ' ';
 		}
 	}
 
-	public void check() { //creating method to select x & O
+	public void check() { // creating method to select x & O
 		System.out.println("Please Select Your Choice Letter : 'X' or 'O' ");
 		char choice = scan.next().charAt(0);
 		if (choice == 'X') {
@@ -25,14 +27,18 @@ public class TicTacToe {
 		}
 	}
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to TIC_TAC_TOE game");
+	public void showboard() {      // creating board
 		System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
 		System.out.println("---------");
-		System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+		System.out.println(board[4] + " | " + board[5] + " | " + board[6]); 
 		System.out.println("---------");
 		System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
-		
+	}
+
+	public static void main(String[] args) {
+		 TicTacToe obj = new TicTacToe();
+	        obj.initialize();
+	        obj.showboard();
 
 	}
 }
