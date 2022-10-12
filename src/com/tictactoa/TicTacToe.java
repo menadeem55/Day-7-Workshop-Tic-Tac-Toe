2c12e99e4f17872aed2ab186b7c95e2cf1b05d67
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-10-12 22:45:16 +0530

    [Added: Choosing X and O]

--- src/com/tictactoa/TicTacToe.java
+++ src/com/tictactoa/TicTacToe.java
@@ -1,17 +1,38 @@
 package com.tictactoa;
-
+import java.util.*;
 public class TicTacToe {
 
-	private static char[] board = new char[10];
-	
-	static void startGame() {
-		for (int i = 1; i <= 9; i++) {
-			board[i] = 1;
-			System.out.println(board);
+	int i;
+	static char[] board = new char[10];//taking array
+	static char player, computer;
+	Scanner scan = new Scanner(System.in);//creating object
+
+	public void initialize() {
+		for (i = 1; i < 10; i++) {   //initialize
+			board[i] = ' ';
 		}
 	}
+
+	public void check() { //creating method to select x & O
+		System.out.println("Please Select Your Choice Letter : 'X' or 'O' ");
+		char choice = scan.next().charAt(0);
+		if (choice == 'X') {
+			player = 'X';
+			computer = 'O';
+		} else if (choice == 'Y') {
+			player = 'O';
+			computer = 'X';
+		}
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Tic Tac Toe ");
-		startGame();
+		System.out.println("Welcome to TIC_TAC_TOE game");
+		System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+		System.out.println("---------");
+		System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+		System.out.println("---------");
+		System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+		
+
 	}
 }
